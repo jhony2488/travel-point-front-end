daeb29c2ac742824124573ee2b6b6266366b9c6e
jhony2488 <jhon.araujo2488@gmail.com>
2023-12-14 18:41:22 -0300

    reconfig back url

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const token='Bearer eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4NDQyMzA0NSwiaWF0IjoxNjg0NDIzMDQ1fQ.3GDYc4YE0FhkUqz1vEaMAfASfbJbJll76yDt-h93fNo'
 
 const api = axios.create({
-    baseURL: 'http://localhost',
+    baseURL: 'https://travel-manager-k9cw.onrender.com',
 });
 
 api.defaults.headers.post['Content-Type'] = 'application/json';
