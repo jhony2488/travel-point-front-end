5219436facd5d26d68650d0809a6175edb01fed2
jhony2488 <jhon.araujo2488@gmail.com>
2023-12-14 16:48:53 -0300

    resolvido conflitos de deprendencias

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import { ThemeProvider } from '@material-ui/core';
 import theme from './providers/theme-service';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
      <ThemeProvider theme={theme}>
                 <App />
         </ThemeProvider>,
+        document.getElementById('root')
 )
